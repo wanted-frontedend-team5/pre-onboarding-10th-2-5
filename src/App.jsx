2cad1200080127d79b7a35a0db76bcc9170c951b
doctork4 <doctor.k1994@gmail.com>
2023-05-03 12:20:23 +0900

    chore : 기본 마크업 & button 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-const App = () => {
+import { Button } from 'component/Button';
+import { useState } from 'react';
 
+const App = () => {
   // todo : 기업 정보가 노출되면 안됩니다 .env를 사용해서 url을 써주세요.
-  const type = 'submit';
+  const buttonType = 'submit';
+  // eslint-disable-next-line no-unused-vars
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <div>
-      <header>init project</header>
-      <button type={type}>123</button>
+      <h2>국내 모든 임상시험 검색하고 온라인으로 참여하기</h2>
+      <form>
+        <label htmlFor="searchDisease">
+          질환명 검색창
+          <input
+            id="searchDisease"
+            placeholder="질환명을 입력해 주세요."
+            value={inputValue}
+          />
+        </label>
+        <Button type={buttonType}>검색</Button>
+      </form>
     </div>
   );
 };

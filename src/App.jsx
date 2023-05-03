93c587d36360bb3ac979595be97266f163e877a7
doctork4 <doctor.k1994@gmail.com>
2023-05-03 14:28:54 +0900

    style : GlobalStyle 컴포넌트 생성 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from 'component/GlobalStyle';
 import { SearchWrap } from 'component/SearchWrap';
 
 const App = () => {
-  return <SearchWrap />;
+  return (
+    <>
+      <GlobalStyle />
+      <SearchWrap />
+    </>
+  );
 };
 
 export default App;

65d0e627afd331d34aa5d431bcca32698ed5caed
yeongi <cbg5255@gmail.com>
2023-05-03 17:32:41 +0900

    feat : focus여부에 따라 검색 리스트 렌더링

--- src/component/search/SearchForm.jsx
+++ src/component/search/SearchForm.jsx
@@ -3,11 +3,18 @@ import { SEARCH } from 'constant/search/search';
 import { STATUS_CODE } from 'constant/api/statusCode';
 import { getSearchResList } from 'api/search';
 import { SearchedItemList } from './SearchedItemList';
+import {
+  InputBox,
+  SearchButton,
+  SearchInput,
+  SearchedListBox,
+} from './style/SearchStyle';
 
 export const SearchForm = () => {
   const [curKeyword, setKeyword] = useState('');
   const [keyWordList, setKeywordList] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [isShowList, setShow] = useState(false);
 
   const getChangeKeywordList = async keyword => {
     const res = await getSearchResList(keyword);
@@ -31,23 +38,39 @@ export const SearchForm = () => {
     e.preventDefault();
   };
 
+  const onFocusSearchInput = () => {
+    setShow(true);
+  };
+
+  const onBlurSearchInput = () => {
+    setShow(false);
+  };
+
   return (
     <>
       <form onSubmit={onSubmitHandler}>
-        <h1>{SEARCH.INPUT_HEADER}</h1>
-        <input
-          type="text"
-          name="search"
-          value={curKeyword}
-          onChange={onChangeSearchInput}
-        />
-        <button type="submit">검색하기</button>
+        <InputBox>
+          <SearchInput
+            type="text"
+            name="search"
+            value={curKeyword}
+            onChange={onChangeSearchInput}
+            onFocus={onFocusSearchInput}
+            onBlur={onBlurSearchInput}
+            placeholder={SEARCH.MESSAGE.INPUT_PLACEHOLDER}
+          />
+          <SearchButton type="submit">🔍</SearchButton>
+        </InputBox>
       </form>
-      <SearchedItemList
-        curkeyword={curKeyword}
-        isLoading={isLoading}
-        keywordList={keyWordList}
-      />
+      {isShowList && (
+        <SearchedListBox>
+          <SearchedItemList
+            curkeyword={curKeyword}
+            isLoading={isLoading}
+            keywordList={keyWordList}
+          />
+        </SearchedListBox>
+      )}
     </>
   );
 };

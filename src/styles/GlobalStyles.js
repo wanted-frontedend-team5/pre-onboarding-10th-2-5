9b5cfcbb88594546e407a9e52473901d99a99683
정원 <66169832+JayeHa@users.noreply.github.com>
2023-05-03 22:51:15 +0900

    feat: Add styled-reset and Implement GlobalStyles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    margin: 0;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+  html {
+    line-height: 1.15; /* 1 */
+    -webkit-text-size-adjust: 100%; /* 2 */
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    border: 0;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  button {
+    padding: 0;
+  }
+
+  ul,
+  ol {
+    padding-left: 0;
+    list-style: none;
+  }
+
+  address {
+    font-style: normal;
+  }
+`;

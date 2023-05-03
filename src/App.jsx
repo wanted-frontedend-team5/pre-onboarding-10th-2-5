dceff4e61380e2d00724b123a6845f306de7a1a2
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-05-03 11:05:41 +0900

    feat: UI 기본 폴더구조 설정 및 GlobalStyle지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-const App = () => {
+import { GlobalStyle } from 'common/GlobalStyle';
+import { Header } from 'components/Header';
 
+const App = () => {
   // todo : 기업 정보가 노출되면 안됩니다 .env를 사용해서 url을 써주세요.
-  const type = 'submit';
   return (
     <div>
-      <header>init project</header>
-      <button type={type}>123</button>
+      <GlobalStyle />
+      <Header />
     </div>
   );
 };

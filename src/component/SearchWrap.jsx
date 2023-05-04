e909aa3bb3ac3e669ba00fe5fc17ac1d3577e658
doctork4 <doctor.k1994@gmail.com>
2023-05-04 12:05:47 +0900

    feat : 추천 검색어가 없는 경우 로컬 캐싱 하지 않는 기능 추가

--- src/component/SearchWrap.jsx
+++ src/component/SearchWrap.jsx
@@ -5,6 +5,7 @@ import {
   Input,
   RecommendUnit,
   KeywordRecommendWindow,
+  Wrapper,
 } from 'component/';
 import { useEffect, useState } from 'react';
 
@@ -23,39 +24,46 @@ export const SearchWrap = () => {
       if (inputValue && !inputValueCache) {
         const newList = await getRecommendData(inputValue);
         setRecommendList(newList);
-        localStorage.setItem(inputValue, JSON.stringify(newList));
+        if (newList.length > 0) {
+          localStorage.setItem(inputValue, JSON.stringify(newList));
+        }
       }
-
-      if (inputValueCache) {
+      if (inputValue && inputValueCache) {
         setRecommendList(JSON.parse(inputValueCache));
       }
     }, 300);
     return () => {
       clearTimeout(debounce);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputValue]);
 
   return (
-    <>
+    <Wrapper>
       <section>
-        <h2>국내 모든 임상시험 검색하고 온라인으로 참여하기</h2>
+        <h2>
+          국내 모든 임상시험 검색하고
+          <br /> 온라인으로 참여하기
+        </h2>
       </section>
-      <form>
-        <Input
-          placeholder="질환명을 입력해 주세요."
-          value={inputValue}
-          onChange={handleValue}
-        />
-        <Button type={buttonType}>검색</Button>
-      </form>
+      <div className="inputWrapper">
+        <form>
+          <Input
+            placeholder="질환명을 입력해 주세요."
+            value={inputValue}
+            onChange={handleValue}
+          />
+          <Button type={buttonType}>검색</Button>
+        </form>
+      </div>
       <KeywordRecommendWindow>
-        {inputValue
-          ? recommendList.map(item => (
-              <RecommendUnit key={item.id}>{item.name}</RecommendUnit>
-            ))
-          : null}
+        {inputValue ? (
+          recommendList.map(item => (
+            <RecommendUnit key={item.id}>{item.name}</RecommendUnit>
+          ))
+        ) : (
+          <RecommendUnit>검색어 없음</RecommendUnit>
+        )}
       </KeywordRecommendWindow>
-    </>
+    </Wrapper>
   );
 };

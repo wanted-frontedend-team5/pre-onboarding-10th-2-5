4b179376f286271a40bb56e73fcd3bfb800fe77e
yeongi <cbg5255@gmail.com>
2023-05-05 21:30:56 +0900

    fix: recommendationsList 조건 추가

--- src/components/SearchBar/Recommendation/RecommendationList/RecommendationList.jsx
+++ src/components/SearchBar/Recommendation/RecommendationList/RecommendationList.jsx
@@ -8,22 +8,23 @@ export const RecommendationList = ({
 }) => {
   return (
     <Styled.List>
-      {recommendations?.map((recommend, index) => {
-        return (
-          <li key={recommend.id}>
-            <Styled.Button
-              isActive={index + 1 === focusIndex}
-              type="button"
-              onClick={() => onClick()}
-              onMouseOver={() => setFocusIndex(index + 1)}
-            >
-              <Styled.SearchIcon />
+      {recommendations.length > 0 &&
+        recommendations.map((recommend, index) => {
+          return (
+            <li key={recommend.id}>
+              <Styled.Button
+                isActive={index + 1 === focusIndex}
+                type="button"
+                onClick={() => onClick()}
+                onMouseOver={() => setFocusIndex(index + 1)}
+              >
+                <Styled.SearchIcon />
 
-              {recommend.name}
-            </Styled.Button>
-          </li>
-        );
-      })}
+                {recommend.name}
+              </Styled.Button>
+            </li>
+          );
+        })}
     </Styled.List>
   );
 };
